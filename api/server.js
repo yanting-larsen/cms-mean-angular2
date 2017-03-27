8fde5bffd3863aeb550191ad896520812ad9b76e
Yanting Larsen <yanting.larsen@gmail.com>
2017-03-27 21:19:30 +0200

    RESTful server

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var morgan = require("morgan");
 var mongodb = require("mongodb");
 var ObjectID = mongodb.ObjectID;
 
 var app = express();
 app.use(bodyParser.json());
+app.use(morgan('combined'));
 
 var db;
 var PAGES_COLLECTION = "pages";
@@ -71,15 +73,36 @@ app.post("/api/pages", function(req, res) {
  *    DELETE: deletes contact by id
  */
 
- app.get("/api/pages/:id", function(req, res) {
+app.get("/api/pages/:id", function(req, res) {
+  db.collection(PAGES_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to get page");
+    } else {
+      res.status(200).json(doc);
+    }
+  });
+});
 
+app.put("/api/pages/:id", function(req, res) {
+   var updateDoc = req.body;
+   delete updateDoc._id;
 
- });
-
- app.put("/api/pages/:id", function(req, res) {
-
+   db.collection(PAGES_COLLECTION).updateOne({_id: new ObjectID(req.params.id) }, updateDoc, function(err, doc) {
+     if (err) {
+       handleError(res, err.message, "Failed to update page");
+     } else {
+       updateDoc._id = req.params.id;
+       res.status(200).json(updateDoc);
+     }
+   });
  });
 
  app.delete("/api/pages/:id", function(req, res){
-
+   db.collection(PAGES_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+     if (err) {
+       handleError(res, err.message, "Failed to delete page");
+     } else {
+       res.status(200).json(req.params.id);
+     }
+   });
  });
